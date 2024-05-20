b3e74f2a91adb4df6289860acdf32371e0623d29
olegmakeienko <molva83@gmail.com>
2024-05-20 20:46:04 +0200

    planet details temporarily

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -1,10 +1,25 @@
 import './styles/planet.css';
+import PlanetType from "../models/planetType.ts";
 
-function Planet() {
+type PlanetProps = {
+    planet: PlanetType;
+};
+
+function Planet({ planet }: PlanetProps) {
     return (
-        <article className="planet">
-            My Planet
-        </article>
+        <div className="planet">
+            <h2>{planet.name} ({planet.latinName})</h2>
+            <p>{planet.desc}</p>
+            <ul>
+                <li>Distance: {planet.distance} km</li>
+                <li>Circumference: {planet.circumference} km</li>
+                <li>Rotation: {planet.rotation} days</li>
+                <li>Orbital Period: {planet.orbitalPeriod} days</li>
+                <li>Temperature: {planet.temp.day}°C day / {planet.temp.night}°C night</li>
+                <li>Type: {planet.type}</li>
+                <li>Moons: {planet.moons.length}</li>
+            </ul>
+        </div>
     );
 }
 
