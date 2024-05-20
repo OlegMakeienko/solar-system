94e8d5b2bc83862059d0a4da9f0fda6280cc5dd3
olegmakeienko <molva83@gmail.com>
2024-05-20 20:46:38 +0200

    PlanetList created

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -1,10 +1,23 @@
 import './styles/planetList.css';
 import Planet from "./Planet.tsx";
+import PlanetType from "../models/planetType.ts";
 
-function PlanetList() {
+type PlanetsProps = {
+    planets : PlanetType[]
+}
+
+function PlanetList({ planets }: PlanetsProps) {
+    console.log("PlanetList planets:", planets); //kontrolerar datan i consolen
+
+    if (!Array.isArray(planets)) {
+        return <div>No planets available</div>;
+    }
+
     return (
         <section className="planet-list">
-            <Planet />
+            {planets.map((planet) => (
+                <Planet planet={planet} key={planet.id} />
+            ))}
         </section>
     );
 }
