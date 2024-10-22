e98f9f697761f002d940699b0b4149e2e8f9d8f4
olegmakeienko <molva83@gmail.com>
2024-10-22 10:48:02 +0200

    eslint.config.js added

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+        rules: {
+            // Ditt ESLint-regler här
+        },
+        // Eventuella andra inställningar
+    },
+];
