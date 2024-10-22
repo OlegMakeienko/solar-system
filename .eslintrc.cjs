996b94027426464623fcfc729ddf358ffd42905f
olegmakeienko <molva83@gmail.com>
2024-10-22 10:53:56 +0200

    jobbar med lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,22 @@
-// eslint.config.js
-import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import typescript from '@typescript-eslint/eslint-plugin';
-
-export default [
-  js.configs.recommended, // Rekommenderade ESLint-regler för JavaScript
-  {
-    files: ['**/*.{ts,tsx}'], // Applicera på alla TypeScript-filer
-    plugins: {
-      '@typescript-eslint': typescript, // Använder TypeScript ESLint-plugin
-    },
-    rules: {
-      'no-unused-vars': 'error', // Fel om oanvända variabler
-      '@typescript-eslint/no-unused-vars': 'error', // Typkontrollerade regler för oanvända variabler
-      'react-hooks/rules-of-hooks': 'error', // Fel om hooks inte används korrekt
-      'react-hooks/exhaustive-deps': 'warn', // Varning för ouppfyllda beroenden i hooks
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    // Ditt anpassade ESLint-regler här
+  },
+  settings: {
+    react: {
+      version: 'detect', // För att automatiskt detektera React-versionen
     },
   },
-  {
-    plugins: {
-      react, // Använder React ESLint-plugin
-    },
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 2021, // Modern JavaScript
-        sourceType: 'module', // ES-moduler
-        ecmaFeatures: {
-          jsx: true, // Aktivera JSX-stöd
-        },
-      },
-      globals: {
-        React: 'writable', // Använd React globalt i projektet utan explicit import
-      },
-    },
-  },
-];
+};
